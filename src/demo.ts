a340b210cabe732a80f1e715e2c12e87e9da5d32
刘鹏 <2254842466@qq.com>
2025-03-30 21:23:44 +0800

    扫描与解析ts文件

---
+++ src/demo.ts
@@ -0,0 +1,14 @@
+import { app } from 'framework';
+
+const dataLen = 3;
+let name = 'iceman';
+
+function doWell() {
+  const app = 4;
+  return app;
+}
+
+function getInfos(info: string) {
+  const result = app.get(info);
+  return result;
+}
